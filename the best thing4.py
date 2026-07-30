602d0818dafd635ed0700c01e25d8d2c24af4dc2
mohamedmhmuod5555-design <mohamedmhmuod5555@gmail.com>
2026-07-30 13:10:25 -0700

    Update the best thing4.py

--- the best thing4.py
+++ the best thing4.py
@@ -49,6 +49,7 @@ if st.session_state.feed=="correct":
   st.session_state.num1=random.randint(1,st.session_state.ran)
   st.session_state.num2=random.randint(1,st.session_state.ran)
   st.session_state.sign=random.choice(['+','-','*','/'])
+  time.sleep(4)
   st.rerun()
 if st.session_state.feed=="false":
    st.error(f"اجابتك خاطئة! الإجابة الصحيحة كانت : {sc}")
@@ -56,7 +57,7 @@ if st.session_state.feed=="false":
    st.session_state.num1=random.randint(1,st.session_state.ran)
    st.session_state.num2=random.randint(1,st.session_state.ran)
    st.session_state.sign=random.choice(['+','-','*','/'])
-   st.spinner()
+   time.sleep(4)
    st.rerun()
 
 if st.session_state.num > 0 and st.session_state.num % 10 == 0:
